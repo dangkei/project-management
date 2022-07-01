2a510bae176764358132bb68319344c0e633225d
Feng Huang <dangkei@hotmail.com>
2022-07-01 18:24:56 +0800

    update

--- project-service/src/main/java/com/bgp/project/bean/Project.java
+++ project-service/src/main/java/com/bgp/project/bean/Project.java
@@ -27,9 +27,9 @@ public class Project implements Serializable {
 	@TableField("project_name")
 	private String projectName;
 	@TableField("construction_method")
-	private ConstructionEnum constructionMethod;
+	private String constructionMethod;
 	@TableField("exploration_method")
-	private ExplorationEnum explorationMethod;
+	private String explorationMethod;
 	@TableField("team_name")
 	private String teamName;
 	@TableField("country")
@@ -47,8 +47,178 @@ public class Project implements Serializable {
 	@TableField("planed_end_date")
 	private String planedEndDate;
 	@TableField("iskey_program")
-	private boolean iskeyProgram;
+	private String iskeyProgram;
 	@TableField("project_status")
 	private String projectStatus;
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the projectName
+	 */
+	public String getProjectName() {
+		return projectName;
+	}
+	/**
+	 * @param projectName the projectName to set
+	 */
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	/**
+	 * @return the constructionMethod
+	 */
+	public String getConstructionMethod() {
+		return constructionMethod;
+	}
+	/**
+	 * @param constructionMethod the constructionMethod to set
+	 */
+	public void setConstructionMethod(String constructionMethod) {
+		this.constructionMethod = constructionMethod;
+	}
+	/**
+	 * @return the explorationMethod
+	 */
+	public String getExplorationMethod() {
+		return explorationMethod;
+	}
+	/**
+	 * @param explorationMethod the explorationMethod to set
+	 */
+	public void setExplorationMethod(String explorationMethod) {
+		this.explorationMethod = explorationMethod;
+	}
+	/**
+	 * @return the teamName
+	 */
+	public String getTeamName() {
+		return teamName;
+	}
+	/**
+	 * @param teamName the teamName to set
+	 */
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+	/**
+	 * @param country the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	/**
+	 * @return the countryCode
+	 */
+	public String getCountryCode() {
+		return countryCode;
+	}
+	/**
+	 * @param countryCode the countryCode to set
+	 */
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	/**
+	 * @return the clientName
+	 */
+	public String getClientName() {
+		return clientName;
+	}
+	/**
+	 * @param clientName the clientName to set
+	 */
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	/**
+	 * @return the longitude
+	 */
+	public double getLongitude() {
+		return longitude;
+	}
+	/**
+	 * @param longitude the longitude to set
+	 */
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	/**
+	 * @return the dimension
+	 */
+	public double getDimension() {
+		return dimension;
+	}
+	/**
+	 * @param dimension the dimension to set
+	 */
+	public void setDimension(double dimension) {
+		this.dimension = dimension;
+	}
+	/**
+	 * @return the planedStartDate
+	 */
+	public String getPlanedStartDate() {
+		return planedStartDate;
+	}
+	/**
+	 * @param planedStartDate the planedStartDate to set
+	 */
+	public void setPlanedStartDate(String planedStartDate) {
+		this.planedStartDate = planedStartDate;
+	}
+	/**
+	 * @return the planedEndDate
+	 */
+	public String getPlanedEndDate() {
+		return planedEndDate;
+	}
+	/**
+	 * @param planedEndDate the planedEndDate to set
+	 */
+	public void setPlanedEndDate(String planedEndDate) {
+		this.planedEndDate = planedEndDate;
+	}
+	/**
+	 * @return the iskeyProgram
+	 */
+	public String isIskeyProgram() {
+		return iskeyProgram;
+	}
+	/**
+	 * @param iskeyProgram the iskeyProgram to set
+	 */
+	public void setIskeyProgram(String iskeyProgram) {
+		this.iskeyProgram = iskeyProgram;
+	}
+	/**
+	 * @return the projectStatus
+	 */
+	public String getProjectStatus() {
+		return projectStatus;
+	}
+	/**
+	 * @param projectStatus the projectStatus to set
+	 */
+	public void setProjectStatus(String projectStatus) {
+		this.projectStatus = projectStatus;
+	}
+	
+	
 	
 }
