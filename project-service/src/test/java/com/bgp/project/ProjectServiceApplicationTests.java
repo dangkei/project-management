9a8a7923de492a2003b5fdf54e85130f3665a2e4
Feng Huang <dangkei@163.com>
2022-07-04 16:11:47 +0800

    11

--- project-service/src/test/java/com/bgp/project/ProjectServiceApplicationTests.java
+++ project-service/src/test/java/com/bgp/project/ProjectServiceApplicationTests.java
@@ -3,17 +3,15 @@ package com.bgp.project;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bgp.project.bean.Project;
 import com.bgp.project.mapper.ProjectMapper;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 class ProjectServiceApplicationTests {
 	
