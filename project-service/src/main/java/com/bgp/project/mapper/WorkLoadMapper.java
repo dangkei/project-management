d2a7e351fada6c03a6060bda1d2237e63c7b4d24
Feng Huang <dangkei@163.com>
2022-07-07 21:49:50 +0800

    update 202207

---
+++ project-service/src/main/java/com/bgp/project/mapper/WorkLoadMapper.java
@@ -0,0 +1,11 @@
+package com.bgp.project.mapper;
+
+import org.springframework.stereotype.Repository;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bgp.project.entity.WorkLoad;
+
+@Repository
+public interface WorkLoadMapper extends BaseMapper<WorkLoad> {
+
+}
